cd417113be9c56b7c1b607b2776142c112bd823d
hromiken <hromiken@gmail.com>
2025-08-29 16:53:59 +0300

    L1

---
+++ task4.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"os"
+	"os/signal"
+	"sync"
+	"time"
+)
+
+func main() {
+	workers := flag.Int("workers", 2, "Number of workers")
+	flag.Parse()
+
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, os.Interrupt)
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	go func() {
+		<-sig
+		cancel()
+	}()
+
+	job := make(chan int, 100)
+	var wg sync.WaitGroup
+
+	for i := 0; i < *workers; i++ {
+		wg.Add(1)
+		go work(&wg, job, ctx, i)
+	}
+
+	data := 0
+	for {
+		select {
+		case <-ctx.Done():
+			fmt.Println("Получен сигнал, закрываем job...")
+			close(job)
+			wg.Wait()
+			fmt.Println("Все воркеры завершены")
+			return
+		case job <- data:
+			data++
+			time.Sleep(1 * time.Second)
+		}
+	}
+
+}
+
+func work(wg *sync.WaitGroup, job chan int, ctx context.Context, i int) {
+	defer wg.Done()
+	for {
+		select {
+		case <-ctx.Done():
+			fmt.Printf("worker %d: завершаюсь\n", i)
+			return
+		case data := <-job:
+			fmt.Printf("worker %d получил задачу: %d\n", i, data)
+		}
+	}
+}
