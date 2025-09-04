760ce5a11b6069b7647d306294be74a8d225e2b1
hromiken <hromiken@gmail.com>
2025-09-04 21:11:34 +0300

    L1.10

--- task10.go
+++ task10.go
@@ -7,13 +7,19 @@ import (
 
 func main() {
 	slice := []float64{-25.4, -27.0, 13.0, 19.0, 15.5, 24.5, -21.0, 32.5}
-	fmt.Println(makeGroup(slice))
+	step := 10.0
+
+	grouped := makeGroup(slice, step)
+	fmt.Println(grouped)
 }
 
-func makeGroup(slice []float64) map[int][]float64 {
+func makeGroup(slice []float64, step float64) map[int][]float64 {
 	m := make(map[int][]float64)
 	for _, v := range slice {
-		group := int(math.Floor(v/10) * 10)
+		group := int(math.Floor(v/step) * step)
+		if v < 0 && float64(group) != v {
+			group += int(step)
+		}
 		m[group] = append(m[group], v)
 	}
 	return m
