acd6ae12e5cc2705126677786ed1e9bbb9c05398
hromiken <hromiken@gmail.com>
2026-03-24 20:16:25 +0300

    L1.20 - теперь операция выполняется не «на месте»

--- task20.go
+++ task20.go
@@ -2,19 +2,33 @@ package main
 
 import (
 	"fmt"
-	"strings"
 )
 
 func main() {
-	str1 := "snow dow sun"
-	answer := reverse(str1)
-	fmt.Println(answer)
+	str := "snow dog sun"
+	fmt.Println(reverseWords(str))
 }
 
-func reverse(str string) string {
-	words := strings.Fields(str)
-	for i, j := 0, len(words)-1; i < j; i, j = i+1, j-1 {
-		words[i], words[j] = words[j], words[i]
+func reverseWords(s string) string {
+	r := []rune(s)
+
+	reverse(r, 0, len(r)-1)
+
+	start := 0
+	for i := 0; i <= len(r); i++ {
+		if i == len(r) || r[i] == ' ' {
+			reverse(r, start, i-1)
+			start = i + 1
+		}
 	}
-	return strings.Join(words, " ")
+
+	return string(r)
+}
+
+func reverse(r []rune, i, j int) {
+	for i < j {
+		r[i], r[j] = r[j], r[i]
+		i++
+		j--
+	}
 }
