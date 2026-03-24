a78f6fe409f9925093bae1cff4694a3049a0d596
hromiken <hromiken@gmail.com>
2026-03-24 13:45:46 +0300

    L1.8 - добавил проверку валидности битов и их индекса. Исправил индексацию.

--- task8.go
+++ task8.go
@@ -4,16 +4,32 @@ import "fmt"
 
 func main() {
 	var number int64 = 5
-	var position uint = 0
+	var position uint = 1
 	bitValue := 0
 
-	number = set(number, position, bitValue)
-	fmt.Println(number)
+	result, err := set(number, position, bitValue)
+	if err != nil {
+		fmt.Println("Ошибка:", err)
+		return
+	}
+
+	fmt.Println(result)
 }
 
-func set(number int64, position uint, bitValue int) int64 {
-	if bitValue == 1 {
-		return number | (int64(1) << position)
+func set(number int64, position uint, bitValue int) (int64, error) {
+	if bitValue != 0 && bitValue != 1 {
+		return 0, fmt.Errorf("бит может быть только 0 или 1")
 	}
-	return number &^ (int64(1) << position)
+
+	if position == 0 || position > 64 {
+		return 0, fmt.Errorf("позиция должна быть от 1 до 64")
+	}
+
+	pos := position - 1
+
+	if bitValue == 1 {
+		return number | (int64(1) << pos), nil
+	}
+
+	return number &^ (int64(1) << pos), nil
 }
