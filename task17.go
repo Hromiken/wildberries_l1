e60fa682cc828407c3d94ee415ee4e865fd01241
hromiken <hromiken@gmail.com>
2026-03-24 19:52:50 +0300

    L1.17 - бинарный поиск теперь возвращает индекс элемента или -1

--- task17.go
+++ task17.go
@@ -9,13 +9,22 @@ import (
 
 func main() {
 	slice := create()
-	target := rand.Intn(len(slice))
 	sort.Ints(slice)
+
+	target := rand.Intn(len(slice))
+
 	start := time.Now()
-	answer := binarySearch(slice, target)
+	index := binarySearch(slice, target)
 	duration := time.Since(start)
-	fmt.Printf("Target is number %d behind 0 and %d\n", target, len(slice)-1)
-	fmt.Printf("Sucsess = %v\n", answer)
+
+	fmt.Printf("Ищем число: %d\n", target)
+
+	if index != -1 {
+		fmt.Printf("Найден индекс: %d\n", index)
+	} else {
+		fmt.Println("Элемент не найден")
+	}
+
 	fmt.Printf("Time for binarySearch = %s\n", duration.String())
 }
 
@@ -28,18 +37,22 @@ func create() []int {
 	return slice
 }
 
-func binarySearch(slice []int, target int) bool {
+func binarySearch(slice []int, target int) int {
 	low, high := 0, len(slice)-1
+
 	for low <= high {
 		mid := (low + high) / 2
+
 		if slice[mid] == target {
-			return true
+			return mid
 		}
+
 		if slice[mid] > target {
 			high = mid - 1
 		} else {
 			low = mid + 1
 		}
 	}
-	return false
+
+	return -1
 }
